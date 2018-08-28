b06dad4382918439596de13cec12b01ad413de5a
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-28 19:49:24 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,51 @@
+def using_push(arr,ele)
+  arr.push(ele)
+end
+
+def using_unshift(arr,ele)
+  arr.unshift(ele)
+end
+
+def using_pop(arr)
+  arr.pop()
+end
+
+def pop_with_args(arr)
+  arr.pop(2)
+end
+
+def using_shift(arr)
+  arr.shift()
+end
+
+def shift_with_args(arr)
+  arr.shift(2)
+end
+
+def using_concat(arr1,arr2)
+  arr1.concat(arr2)
+end
+
+def using_concat(arr1,arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(arr,ele)
+  arr.insert(4,ele)
+end
+
+def using_uniq(arr)
+  arr.uniq()
+end
+
+def using_flatten(arr)
+  arr.flatten()
+end
+
+def using_delete(arr,ele)
+  arr.delete(ele)
+end
+
+def using_delete_at(arr,index)
+  arr.delete(arr[index])
+end
